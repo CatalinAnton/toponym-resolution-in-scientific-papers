12381be3344328f8455fd3d6f11028b2ee213734
Alex Mihaes <alex.mihaes@icloud.com>
2019-05-13 13:03:13 +0300

    Created dictionary of GeoNames

--- p1_file_management/input.py
+++ p1_file_management/input.py
@@ -22,9 +22,29 @@ def get_dictionary_file_content(files):
 
 resource_location_output = '..\\output_final'
 
+
 def get_file_list_output_final():
     files = []
     for root, dirnames, filenames in os.walk(resource_location_output):
         for filename in filenames:
             files.append(os.path.join(root, filename))
     return files
+
+
+def get_dictionary_geo_names():
+    with open("..\\resources\\geo_names\\allCountries.txt", 'r', encoding='utf8', errors='ignore') as geo_names_file:
+        line = geo_names_file.readline()
+        cnt = 1
+        dictionary_geo_names = {}
+        while line:
+            line_splitted = line.strip().split('\t')
+            toponym = line_splitted[1]
+            location = line_splitted[17]
+            latitude = line_splitted[4]
+            longitude = line_splitted[5]
+            dictionary_geo_names[toponym] = {"location": location, "latitude": latitude, "longitude": longitude}
+            line = geo_names_file.readline()
+            if(cnt % 1000000 == 0):
+                print(cnt)
+            cnt += 1
+        return dictionary_geo_names
